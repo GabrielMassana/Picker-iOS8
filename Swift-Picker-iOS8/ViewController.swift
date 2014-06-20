92c1984ab36ffcd6201f187ebdcc30ee2aae5ba0
Gabriel Massana <Gabi@LOCK8s-Mac-mini.local>
2014-06-20 14:46:31 +0100

    Initial Commit

---
+++ Swift-Picker-iOS8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Picker-iOS8
+//
+//  Created by Gabriel Massana on 20/06/2014.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
